48250395f44b570120da97ec947d3b5ec7d5e803
nils <nillenice45@gmail.com>
2018-09-08 18:02:08 +0200

    ja

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class helloworld {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int x = input.nextInt();
+		int y = input.nextInt();
 
-}
+		if(x>0 && y>0) {
+			System.out.println(1);
+	}
+		if(x>0 && y<0) {
+			System.out.println(4);
+		}
+		if(x<0 && y>0) {
+			System.out.println(2);
+		}
+		if(x<0 && y<0) {
+			System.out.println(3);
+		}
+	
+	} // END main
+	
+} // END helloworld
