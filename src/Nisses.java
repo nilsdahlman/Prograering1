fc91b4100fc27eaab9a989e0fd3ce64a945b0c24
nils <nillenice45@gmail.com>
2018-09-11 12:38:36 +0200

    aha
    jaja ja

--- src/Nisses.java
+++ src/Nisses.java
@@ -6,7 +6,25 @@ public class Nisses {
 	
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Vad heter du?");
+		String namn = input.nextLine();
+		
+		System.out.println("Hur gammal är du?");
+		String ålder = input.nextLine();
+		
+		System.out.println("Vad är din adress?");
+		String adress = input.nextLine();
+		
+		System.out.println("Vad är ditt postnummer?");
+		String postnummer = input.nextLine();
+		
+		System.out.println("Vilken stad bor du i?");
+		String stad = input.nextLine();
+		
+		System.out.println("Vad är ditt telefonnummer?");
+		String telefon = input.nextLine();
 	}
 
 }
