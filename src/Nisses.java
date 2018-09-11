3cfb4146966c99e1d1d812098c2b5eb43d1d3c99
nils <nillenice45@gmail.com>
2018-09-11 12:45:54 +0200

    klar bby

--- src/Nisses.java
+++ src/Nisses.java
@@ -25,6 +25,14 @@ public class Nisses {
 		
 		System.out.println("Vad är ditt telefonnummer?");
 		String telefon = input.nextLine();
+		
+		input.close();
+		
+		System.out.println("Information:");
+		System.out.println("Namn: " + namn);
+		System.out.println("Ålder: " + ålder);
+		System.out.println("Adress: " + adress + "\br      " + postnummer + stad);
+		System.out.println("Telefon: " + telefon);
 	}
 
 }
