6a4cedc8c1c38a03a8b29ceeff2d8617d9e0e86c
nils <nillenice45@gmail.com>
2018-09-14 14:29:18 +0200

    lol indeed

---
+++ src/lol.java
@@ -0,0 +1,15 @@
+
+public class lol {
+	int max = 999999999999;
+	int min = -999999999999;
+	
+	int tal1 = 5;
+	int tal2 = 4;
+	int tal3 = 7;
+	int tal4 = 5;
+	int tal5 = 9;
+	
+	int summa = tal1+tal2+tal3+tal4+tal5; // 28
+	double medel = summa/5.0
+
+}
