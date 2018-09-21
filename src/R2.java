60b46b78804b2098a75317218c62d85924858fe7
nils <nillenice45@gmail.com>
2018-09-21 14:59:24 +0200

    lolll

---
+++ src/R2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class R2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		
+		double R2;
+		
+		int S = input.nextInt();
+		int R1 = input.nextInt();
+		
+		System.out.println( R1 * 2 - S);
+	}
+}
